b8ef64ac99de4b7d5ace7bced14af06f45dcb6d7
Lucas Ribeiro <79470623+trlucasr@users.noreply.github.com>
2023-09-17 16:26:34 -0300

    convert route get in post - auth

--- ws/src/routes/auth.routes.js
+++ ws/src/routes/auth.routes.js
@@ -20,28 +20,28 @@ router.get('/', async (req, res) => {
   });
   
 
-router.get('/login', async (req, res) => {
-    try {
-      const { email, senha } = req.query;
-  
-      const usuario = await Auth.findOne({ where: { email } });
-  
-      if (!usuario) {
-        return res.json({ error: true, message: 'Email não encontrado' });
-      }
-  
-      const senhaCorrespondente = await bcrypt.compare(senha, usuario.senha);
-  
-      if (!senhaCorrespondente) {
-        return res.json({ error: true, message: 'Senha incorreta' });
-      }
-  
-      // Se a autenticação for bem-sucedida, você pode fazer outras ações aqui
-  
-      res.json({ success: true, message: 'Autenticação bem-sucedida' });
-    } catch (err) {
-      res.status(500).json({ error: true, message: err.message });
+router.post('/login', async (req, res) => {
+  try {
+    const { email, senha } = req.body; // Usamos req.body em vez de req.query
+
+    const usuario = await Auth.findOne({ where: { email } });
+
+    if (!usuario) {
+      return res.json({ error: true, message: 'Email não encontrado' });
     }
-  });
+
+    const senhaCorrespondente = await bcrypt.compare(senha, usuario.senha);
+
+    if (!senhaCorrespondente) {
+      return res.json({ error: true, message: 'Senha incorreta' });
+    }
+
+    // Se a autenticação for bem-sucedida, você pode fazer outras ações aqui
+
+    res.json({ success: true, message: 'Autenticação bem-sucedida' });
+  } catch (err) {
+    res.status(500).json({ error: true, message: err.message });
+  }
+});
 
 module.exports = router;
